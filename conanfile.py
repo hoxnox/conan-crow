01b0a51486e35d13e650c5466742488b30d26d8a
hoxnox <hoxnox@gmail.com>
2019-01-10 23:07:54 +0300

    ENH: 49edf898 version added

--- conanfile.py
+++ conanfile.py
@@ -4,10 +4,10 @@ from conans import tools
 
 class CrowConan(NxConanFile):
     name = "crow"
-    version = "4e39b23e"
+    version = "49edf898"
     license = "BSD"
     url = "https://github.com/hoxnox/conan-crow"
-    license = "https://raw.githubusercontent.com/ipkn/crow/4e39b23e455e455f1878b3e68d729a1737f3e431/LICENSE"
+    license = "https://github.com/ipkn/crow/blob/49edf898a5b9a39a0d69072cc2434c4f23692908/LICENSE"
     settings = "os", "compiler", "build_type", "arch"
     build_policy = "missing"
     description = "Crow is C++ microframework for web. (inspired by Python Flask)"
@@ -15,10 +15,10 @@ class CrowConan(NxConanFile):
     options = {}
 
     def do_source(self):
-        self.retrieve("417064e3a2238c3877877e5ed9902686c87596440d426b893027abd7a5b5a52d",
+        self.retrieve("8863e630403669f9cc4598a3368f50053a98354ad2b85075384df99628b598f0",
                 [
                     "vendor://ipkn/crow/crow-{v}.zip".format(v=self.version),
-                    "https://github.com/ipkn/crow/archive/4e39b23e455e455f1878b3e68d729a1737f3e431.zip"
+                    "https://codeload.github.com/ipkn/crow/zip/49edf898a5b9a39a0d69072cc2434c4f23692908"
                 ],
                 "crow-{v}.zip".format(v=self.version))
 
@@ -26,4 +26,4 @@ class CrowConan(NxConanFile):
         tools.unzip("crow-{v}.zip".format(v=self.version), "{staging_dir}/src".format(staging_dir=self.staging_dir))
 
     def do_package(self):
-        self.copy(pattern="*", dst="include", src="{staging_dir}/src/crow-4e39b23e455e455f1878b3e68d729a1737f3e431/include".format(staging_dir=self.staging_dir))
+        self.copy(pattern="*", dst="include", src="{staging_dir}/src/crow-49edf898a5b9a39a0d69072cc2434c4f23692908/include".format(staging_dir=self.staging_dir))
